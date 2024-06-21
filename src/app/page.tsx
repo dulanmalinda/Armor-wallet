a2ca46bb051780ea66c8d1caf1dbd29dae666054
Dulanmalinda <dulanmalinda@gmail.com>
2024-06-21 16:36:34 +0530

    api update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,7 +11,7 @@ import { createThirdwebClient } from "thirdweb";
 
 import { useEffect, useState } from 'react';
 
-const apiURL = "https://armor-vote-backend-giarkh0sf-dulanmalindas-projects.vercel.app/api/";
+const apiURL = "https://armor-vote-backend.vercel.app/api/";
 
 const Page = () => {
   const [walletAddress, setWalletAddress] = useState<string | null>(null);
